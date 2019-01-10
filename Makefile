910815ba7e90b8559290d920f0addfb213663b48
scriptonist <aravindkp@outlook.in>
2019-01-10 22:33:27 +0530

    Add run to root command

---
+++ Makefile
@@ -0,0 +1,4 @@
+SHELL := "/bin/bash"
+
+termd:
+	go install github.com/scriptonist/termd
--- cmd/root.go
+++ cmd/root.go
@@ -3,10 +3,16 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 
+	"github.com/gdamore/tcell"
 	homedir "github.com/mitchellh/go-homedir"
+	"github.com/rivo/tview"
+	"github.com/russross/blackfriday"
+	"github.com/scriptonist/termd/pkg/console"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -16,6 +22,63 @@ var cfgFile string
 var rootCmd = &cobra.Command{
 	Use:   "termd",
 	Short: "Render markdown on terminal",
+	RunE:  run,
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	var output []byte
+	cr := new(console.Console)
+
+	if len(args) == 1 {
+		fileb, err := ioutil.ReadFile(args[0])
+		if err != nil {
+			return err
+		}
+		output = blackfriday.Run(
+			fileb,
+			blackfriday.WithRenderer(cr),
+			blackfriday.WithExtensions(blackfriday.CommonExtensions),
+		)
+	} else {
+		reader := bufio.NewReader(os.Stdin)
+		var input []byte
+		buffer := make([]byte, 2<<20)
+		for {
+			count, err := reader.Read(buffer)
+			if count == 0 {
+				break
+			}
+			if err != nil {
+				os.Stderr.WriteString(fmt.Sprintf("Unable to read from pipe :%v", err))
+			}
+			input = append(input, buffer...)
+		}
+		output = blackfriday.Run(
+			input,
+			blackfriday.WithRenderer(cr),
+			blackfriday.WithExtensions(blackfriday.CommonExtensions),
+		)
+	}
+	app := tview.NewApplication()
+	textView := tview.NewTextView().
+		SetDynamicColors(true).
+		SetRegions(true).
+		SetChangedFunc(func() {
+			app.Draw()
+		})
+	textView.Write(output)
+
+	textView.SetBorder(true)
+	textView.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEscape {
+			app.Stop()
+		}
+	})
+	if err := app.SetRoot(textView, true).SetFocus(textView).Run(); err != nil {
+		panic(err)
+	}
+
+	return nil
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
